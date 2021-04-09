d26b2cbcc1ce461bfce6c40fd000af3e49dfb803
lorossi <contact@lorenzoros.si>
2021-04-10 00:29:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS Utils
+
+Small collection of useful Javascript/ECMAscript snippets that I constantly need.
---
+++ utils.js
@@ -0,0 +1,58 @@
+const random = (a, b) => {
+  if (a == undefined && b == undefined) return random(0, 1);
+  else if (b == undefined) return random(0, a);
+  else if (a != undefined && b != undefined) return Math.random() * (b - a) + a;
+};
+
+const random_int = (a, b) => {
+  if (a == undefined && b == undefined) return random_int(0, 1);
+  else if (b == undefined) return random_int(0, a);
+  else if (a != undefined && b != undefined) return Math.floor(Math.random() * (b - a + 1)) + a;
+};
+
+const random_interval = (average, interval) => {
+  average = average || 0.5;
+  interval = interval || 0.5;
+  return random(average - interval, average + interval);
+};
+
+const random_from_array = (arr) => {
+  return arr[random_int(arr.length)];
+};
+
+const shuffle_array = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
+const map = (val, old_min, old_max, new_min, new_max) => {
+  return (val - old_min) * (new_max - new_min) / (old_max - old_min) + new_min;
+};
+
+const distSq = (x1, y1, x2, y2) => {
+  return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
+};
+
+const dist = (x1, y1, x2, y2) => {
+  return Math.sqrt(distSq(x1, y1, x2, y1));
+};
+
+const constrain = (x, min_val = 0, max_val = 1) => {
+  return Math.max(Math.min(x, max_val), min_val);
+};
+
+const wrap = (x, min_val = 0, max_val = 1) => {
+  while (x > max_val) x -= max_val - min_val;
+  while (x < min_val) x += max_val - min_val;
+  return x;
+};
+
+const is_mobile = () => {
+  return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+};
+
+const get_css_var = (property) => {
+  return getComputedStyle(document.documentElement).getPropertyValue(property).split(" ").join("");
+}
