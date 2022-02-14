89184a29d050c59dda5394ee1e3f8f15537278b7
lorossi <contact@lorenzoros.si>
2022-02-14 01:25:02 +0100

    fixed a function, lintering

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@
  * Returns a random number in range [a, b) (i.e. a included, b excluded)
  * If only one parameter is passed, the random number will be generated in range [0, a)
  * If no parameters are passed, the random number will be generated in range [0, 1)
- * 
+ *
  * @param {Number} [a] if two parameters are passed, minimum range value; maximum range value otherwise
  * @param {Number} [b] maximum range value
  * @returns {Number} random number
@@ -25,7 +25,8 @@ const random = (a, b) => {
 const random_int = (a, b) => {
   if (a == undefined && b == undefined) return random_int(0, 2);
   else if (b == undefined) return random_int(0, a);
-  else if (a != undefined && b != undefined) return Math.floor(Math.random() * (b - a)) + a;
+  else if (a != undefined && b != undefined)
+    return Math.floor(Math.random() * (b - a)) + a;
 };
 
 /**
@@ -43,7 +44,7 @@ const random_interval = (average = 0.5, interval = 0.5) => {
 
 /**
  * Return a random number generated with a gaussian distribution
- * 
+ *
  * @param {Number} [min=0] minimum value of the random numbers
  * @param {Number} [max=1] minimum value of the random numbers
  * @param {Number} [skew=0] skew of the gaussian function
@@ -73,11 +74,11 @@ const random_normal = (min = 0, max = 1, skew = 0) => {
 
 /**
  * Returns a random item from the provided array
- * 
+ *
  * @param {Array} a an array
  * @returns {any} item from input array
  */
-const random_from_array = a => {
+const random_from_array = (a) => {
   return a[random_int(a.length)];
 };
 
@@ -86,7 +87,7 @@ const random_from_array = a => {
  *
  * @param {Array} a an array
  */
-const shuffle_array = a => {
+const shuffle_array = (a) => {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
@@ -95,7 +96,7 @@ const shuffle_array = a => {
 
 /**
  * Returns the square distance between two coordinates
- * 
+ *
  * @param {Number} x1 first coordinate x value
  * @param {Number} y1 first coordinate y value
  * @param {Number} x2 second coordinate x value
@@ -116,35 +117,37 @@ const dist_sq = (x1, y1, x2, y2) => {
  * @returns {Number} distance between the coordinates
  */
 const dist = (x1, y1, x2, y2) => {
-  return Math.sqrt(dist_sq(x1, y1, x2, y2));
+  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
 
 /**
  * Maps (rescales) a value from an old interval to a new interval
- * 
- * @param {Number} value value in the old interval 
+ *
+ * @param {Number} value value in the old interval
  * @param {Number} old_min minimum value of the old interval
  * @param {Number} old_max maximum value of the old interval
  * @param {Number} new_min minimum value of the new interval
  * @param {Number} new_max maximum value of the new interval
  * @returns {Number} mapped value
- * 
+ *
  * @example
  * map(192, 0, 255, 1024, 2048);
  * // => 1795.0117647058823
  */
 const map = (value, old_min, old_max, new_min, new_max) => {
-  return (value - old_min) * (new_max - new_min) / (old_max - old_min) + new_min;
+  return (
+    ((value - old_min) * (new_max - new_min)) / (old_max - old_min) + new_min
+  );
 };
 
 /**
  * Wraps a value into an interval, like the modulo expression but in both directions of the interval
- * 
+ *
  * @param {Number} value value to be wrapped
  * @param {Number} [min_val=0] minimum value in the interval
  * @param {Number} [max_val=1] maximum value in the interval
- * @returns {Number} 
- * 
+ * @returns {Number}
+ *
  * @example
  * wrap(65, 0, 60);
  * // => 5
@@ -157,12 +160,12 @@ const wrap = (value, min_val = 0, max_val = 1) => {
 
 /**
  * Clamps (constrains) a value into an interval
- * 
+ *
  * @param {Number} value value to be clamped
  * @param {Number} [min_val=0] minimum value in the interval
  * @param {Number} [max_val=1] maximum value in the interval
  * @returns  {Number}
- * 
+ *
  * @example
  * clamp(-5, -3, 10);
  * // => -3
@@ -173,21 +176,26 @@ const clamp = (value, min = 0, max = 1) => {
 
 /**
  * Returns true if the function is called by a mobile browser
- * 
+ *
  * @returns {boolean}
  */
 const is_mobile = () => {
-  return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
 };
 
 /**
  * Returns the value of a CSS variable used in the current page
- * 
+ *
  * @param {String} property CSS property name
  * @returns {String} CSS property value
  */
-const get_css_var = property => {
-  return getComputedStyle(document.documentElement).getPropertyValue(property).split(" ").join("");
+const get_css_var = (property) => {
+  return getComputedStyle(document.documentElement)
+    .getPropertyValue(property)
+    .split(" ")
+    .join("");
 };
 
 /**
@@ -198,11 +206,11 @@ const get_css_var = property => {
 
 /**
  * Returns the (x, y) coordinates of a 1D data structure treated as a 2D structure (a grid)
- * 
+ *
  * @param {Number} i index of the item
  * @param {Number} width width of the data structure
  * @returns {Point}
- * 
+ *
  * @example
  * xy_from_index(25, 10);
  * // => {x: 5, y: 2}
@@ -215,12 +223,12 @@ const xy_from_index = (i, width) => {
 
 /**
  * Returns the index corresponding to the (x, y) coordinates of a 2D data structure (a grid) treated as a 1D structure
- * 
+ *
  * @param {Number} x x coordinate of the point
  * @param {Number} y y coordinate of the point
  * @param {Number} width width of the data structure
  * @returns {Number}
- * 
+ *
  * @example
  * index_from_xy(7, 4, 12);
  * // => 55
@@ -231,21 +239,21 @@ const index_from_xy = (x, y, width) => {
 
 /**
  * Returns the hexadecimal conversion of a decimal number
- * 
+ *
  * @param {Number} dec the base 10 number
  * @param {Number} [padding=0] digits of zero padding in front of the hex number
  * @param {boolean} [prefix=false] if true, 0x gets added in front of the hex number
  * @param {boolean} [round=true] if true, the number gets rounded before conversion
  * @returns {Number}
- * 
+ *
  * @example
  * dec_to_hex(232);
  * // => "E8"
- *  
+ *
  * @example
  * dec_to_hex(12, 2);
  * // => "0C"
- * 
+ *
  * @example
  * dec_to_hex(14, 2, true);
  * // => "0x0E"
@@ -269,7 +277,7 @@ const dec_to_hex = (dec, padding = 0, prefix = false, round = true) => {
  * @example
  * hex_to_dec("0xF6");
  * // => 246
-  * @example
+ * @example
  * hex_to_dec("64h");
  *  100
  */
@@ -277,21 +285,26 @@ const hex_to_dec = (hex) => parseInt(hex, 16);
 
 /**
  * Shuffles and returns a string
- * 
- * @param {String} string the string to be shuffled 
+ *
+ * @param {String} string the string to be shuffled
  * @returns {String}
  */
-const shuffle_string = (string) => string.split("").sort((_, __) => Math.random() * 2 - 1).join("");
+const shuffle_string = (string) =>
+  string
+    .split("")
+    .sort((_, __) => Math.random() * 2 - 1)
+    .join("");
 
 /**
  * Generates a random alphanumeric string of set length
- * 
+ *
  * @param {Number} len length of the random string
  * @returns {String}
  */
 const random_string = (len) => {
   let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split("");
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split("");
   for (let i = 0; i < len; i++) result += random_from_array(characters);
   return result;
 };
